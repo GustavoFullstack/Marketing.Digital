50d7cb4f4cc1c97d1575263513116eb53906580f
Gustavo Almeida FullStack <147890923+GustavoFullstack@users.noreply.github.com>
2023-12-03 12:34:50 -0300

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Gestão.Plataforma
+# Fsociety Comunidade Brasil
 Plataforma para dividir conhecimentos sobre gestão, marketing, programação e muuito mais. 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Hello World</h1>
+    return <h1>Gustavo você é incrivel e falta pouco para seus sonhos se tornarem realidade</h1>
 }
 
 export default Home;
